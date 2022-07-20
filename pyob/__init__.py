fcf77254426ba2e3d09192c54b0697c1bc1ac2b1
khunspoonzi <khunspoonzi@gmail.com>
2022-07-19 22:56:23 -1000

    Nuke PyOb v0

--- pyob/__init__.py
+++
@@ -1,8 +0,0 @@
-# ┌────────────────────────────────────────────────────────────────────────────────────┐
-# │ PROJECT IMPORTS                                                                    │
-# └────────────────────────────────────────────────────────────────────────────────────┘
-
-from pyob.pyob import PyOb
-from pyob.pyob_set import PyObSet
-from pyob.pyob_store import PyObStore
-from pyob.tools import localize
