400d6a6ae13cdaf8f2e995ce980b9e55f0b4676f
khunspoonzi <khunspoonzi@gmail.com>
2022-07-25 23:59:59 -1000

    Improve PyOb class localization

--- pyob/main/tools/__init__.py
+++ pyob/main/tools/__init__.py
@@ -2,8 +2,8 @@
 # │ PROJECT IMPORTS
 # └─────────────────────────────────────────────────────────────────────────────────────
 
+from pyob.main.tools.localize_pyob_class import localize_pyob_class  # noqa
 from pyob.main.tools.set_pyob_attr import set_pyob_attr  # noqa
-from pyob.tools import is_pyob_base, is_pyob_subclass
 
 
 # ┌─────────────────────────────────────────────────────────────────────────────────────
@@ -26,49 +26,3 @@ def get_pyob_string_field(pyob):
 
     # Return the string field
     return string_field
-
-
-# ┌─────────────────────────────────────────────────────────────────────────────────────
-# │ LOCALIZE PYOB CLASS
-# └─────────────────────────────────────────────────────────────────────────────────────
-
-
-def localize_pyob_class(*PyObClasses):
-    """Localizes and returns an interable of PyOb class instances"""
-
-    # Initialize cache
-    cache = {}
-
-    # Define localize helper function
-    def _localize(PyObClass):
-
-        # Return if class is not a PyOb subclass
-        if is_pyob_base(PyObClass) or not is_pyob_subclass(PyObClass):
-            return PyObClass
-
-        # Return if PyOb class in cache
-        if PyObClass in cache:
-            return cache[PyObClass]
-
-        # Localize PyOb bases
-        Bases = tuple([_localize(Base) for Base in PyObClass.__bases__])
-
-        print("1")
-
-        # Get and set localized PyOb class
-        cache[PyObClass] = type(PyObClass.__name__, Bases, dict(PyObClass.__dict__))
-
-        print("2")
-
-        # Set localized from attribute
-        # This will ensure that isinstance() still works with localized PyOb classes
-        cache[PyObClass].PyObMeta.localized_from = PyObClass
-
-        # Return the localized PyOb class
-        return cache[PyObClass]
-
-    # Localize PyOb classes
-    PyObClasses = [_localize(PyObClass) for PyObClass in PyObClasses]
-
-    # Return localized PyOb classes
-    return PyObClasses[0] if len(PyObClasses) == 1 else tuple(PyObClasses)
